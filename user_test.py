e79d37332d56188079467a862322e40c32531911
IanSani <iansani259@gmail.com>
2019-04-08 10:36:47 +0300

    imported unittest in user test file

--- user_test.py
+++ user_test.py
@@ -1,10 +1,14 @@
+import unittest #Importing the unittest module
+import pyperclip #Helps in copy and paste functions
+from user import user # importing the user class
+
 def test_save_user(self):
     """
     test_save_user test case to test if the user object is saved into
     the user user list
     """
     self.new_user.save_user() #saving the new contact
-    self.asserEqual(len(user.user_list),1)
+    self.asserEqual(len(user.users_list),1)
 
 if _name_ == '_main_':
     unittest.main()
